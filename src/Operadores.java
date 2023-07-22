af4cc8b335f9a38308e4157a63355f791f1415dc
nenasantos <g.lorena@live.com>
2023-07-21 22:22:27 -0300

    first commit

---
+++ src/Operadores.java
@@ -0,0 +1,9 @@
+public class Operadores {
+    public static void main (String [] args) {
+        String nomeCompleto = "Lorena" + " Santos";
+        System.out.print(nomeCompleto);
+    }
+
+    
+    
+}
